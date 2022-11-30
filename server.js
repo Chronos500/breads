1c3528772482839603f17059eed763307c9aa071
Hansel <you@example.com>
2022-11-30 18:50:55 -0500

    connected to mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -7,9 +9,14 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+
+
 
 
 // ROUTES
@@ -27,3 +34,13 @@ app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
--- views/index.jsx
+++ views/index.jsx
@@ -25,6 +25,14 @@ function Index ({breads,title})  {
 }
 
        </ul>
+       <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+<div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
+
+
       </Default>
       
 
